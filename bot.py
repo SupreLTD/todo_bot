e299bd00f95f51912dc120e813845f26ef38fa38
Vanya <balanaaarrrr@gmail.com>
2023-12-22 11:16:23 +0300

    start bot

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import asyncio
+from aiogram import Dispatcher, Bot
+import logging
 
+from config import env
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+    )
+    bot = Bot(env("BOT_TOKEN"))
+    dp = Dispatcher()
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
